206d0dde1079d7d41e01632a92b8b6bdceeec47d
Dina Essam <dinaessam@gate7.me>
2020-12-01 14:00:41 +0200

    Add POJOs

---
+++ src/main/java/dina/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package dina.springframework.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/dina/springframework/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package dina.springframework.petclinic.model;
+
+public class Vet extends Person {
+}
